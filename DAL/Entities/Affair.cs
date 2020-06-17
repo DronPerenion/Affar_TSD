b1c5fe81cffd2de686d473ac6977a83b1c48b2f9
Unknown <unknown@example.com>
2020-06-17 13:12:53 +0300

    Add entities to DAL

---
+++ DAL/Entities/Affair.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities
+{
+    public class Affair
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public PoliceDepartment PoliceDepartment { get; set; }
+        public Guid PoliceDepartmentId { get; set; }
+        public string Description { get; set; }
+        public int Section { get; set; }
+        public int Paragraph { get; set; }
+
+    }
+}
---
+++ DAL/Entities/PoliceDepartment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities
+{
+    public class PoliceDepartment
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public Guid RegionId { get; set; }
+        public Region Region { get; set; }
+        public ICollection<Affair> Affairs { get; set; }
+        public string Name { get; set; }
+        public uint Employees { get; set; }
+    }
+}
