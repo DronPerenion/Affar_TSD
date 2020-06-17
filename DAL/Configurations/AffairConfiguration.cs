016bf0b637f24868416c0934c99cf8f2db3738e0
Unknown <unknown@example.com>
2020-06-17 13:13:55 +0300

    Add Context and configuration

---
+++ DAL/Configurations/AffairConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configurations
+{
+    public class AffairConfiguration : IEntityTypeConfiguration<Affair>
+    {
+        public void Configure(EntityTypeBuilder<Affair> builder)
+        {
+            builder.ToTable("Affairs");
+
+            builder.HasIndex(a => a.Id)
+                .IsUnique();
+        }
+    }
+}
---
+++ DAL/Configurations/PoliceDepartmentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configurations
+{
+    public class PoliceDepartmentConfiguration : IEntityTypeConfiguration<PoliceDepartment>
+    {
+        public void Configure(EntityTypeBuilder<PoliceDepartment> builder)
+        {
+            builder.ToTable("PoliceDepartments");
+
+            builder.HasIndex(a => a.Id)
+                .IsUnique();
+
+            builder.HasMany(a => a.Affairs)
+                .WithOne(a => a.PoliceDepartment)
+                .HasForeignKey(a => a.PoliceDepartmentId);
+        }
+    }
+}
