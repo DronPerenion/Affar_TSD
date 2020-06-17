8dea17a72a732221b4e5b53c1332de0e5fb2b7cf
Unknown <unknown@example.com>
2020-06-17 13:15:17 +0300

    Add DTO's to BLL

---
+++ BLL/DTO/AffairDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using DAL.Entities;
+
+namespace BLL.DTO
+{
+    public class AffairDTO
+    {
+        public Guid Id { get; set; }
+        public Guid PoliceDepartmentId { get; set; }
+        public string Description { get; set; }
+        public int Section { get; set; }
+        public int Paragraph { get; set; }
+    }
+}
---
+++ BLL/DTO/PoliceDepartmentDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DAL.Entities;
+
+namespace BLL.DTO
+{
+    public class PoliceDepartmentDTO
+    {
+        public Guid Id { get; set; }
+        public Guid RegionId { get; set; }
+        public Region Region { get; set; }
+        public string Name { get; set; }
+        public uint Employees { get; set; }
+    }
+}
